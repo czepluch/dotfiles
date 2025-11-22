5990ddcd283b3a52894637bdf8e093785a2c1c51
jstcz <j.czepluch@proton.me>
2025-11-22 22:38:54 +0100

    Configure snacks explorer to show hidden files by default

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        files = {
+          hidden = true, -- Show hidden files in file picker
+        },
+        explorer = {
+          hidden = true, -- Show hidden files in explorer by default
+        },
+      },
+    },
+  },
+}
