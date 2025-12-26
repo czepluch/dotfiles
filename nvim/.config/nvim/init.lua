f16a0fefad03ea88edbc295939fdec69364cee98
jstcz <j.czepluch@proton.me>
2025-12-26 22:52:58 +0100

    Fix Neovim Aether integration: use plugin symlink instead of dofile
    - Remove dofile() from init.lua (returns table but doesn't execute)
    - Add symlink to Aether's neovim.lua in plugins directory
    - Update lazy-lock.json with plugin changes

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
--- Load Aether theme (must be after lazy.nvim setup)
-dofile(vim.fn.expand("~/.config/aether/theme/neovim.lua"))
